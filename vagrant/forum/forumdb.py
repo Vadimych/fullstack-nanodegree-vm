f8373daa270f7ce809b1577dee4f6baf0fa753b9
Вадим Логін <login.vadim@gmail.com>
2016-10-23 23:05:18 +0300

    Fix script-injection bug

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,11 +3,14 @@
 # 
 
 import psycopg2
+import bleach
 
 ## Get posts from database.
 def GetAllPosts():
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
+    c.execute("UPDATE posts SET content = '__delete-spam__' WHERE content LIKE '%spam%'")
+    c.execute("DELETE FROM posts WHERE content = '__delete-spam__'")
     c.execute("SELECT time, content FROM posts ORDER BY time DESC")
     posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
     DB.close()
@@ -17,6 +20,6 @@ def GetAllPosts():
 def AddPost(content):
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES (%s)", (content, ))
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (bleach.clean(content), ))
     DB.commit()
     DB.close()
